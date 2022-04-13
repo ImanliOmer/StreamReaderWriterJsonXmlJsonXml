a76b19843c6a5a30fc541d920041e421726f02f4
ImanliOmer <omarzi@code.edu.az>
2022-04-13 17:37:45 +0400

    Upload

---
+++ StreamReaderWriterJsonXml1/StreamReaderWriterJsonXml/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamReaderWriterJsonXml.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private List<Employee> _employees;
+        public List<Employee> Employees 
+        { 
+            get 
+            { return _employees; } set { _employees = value; } }
+        public Department(string name, Employee employees)
+        {
+            Id++;
+            Name = name;
+            _employees.Add(employees);
+        }
+
+    }
+}
